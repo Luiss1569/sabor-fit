96be73cd0684870a429fe14239b6ae56c2b40dbd
Luiss1569 <rcsrcs1569@gmail.com>
2023-07-14 09:53:36 -0300

    :sparkles: create title component title

--- frontend/src/components/Title/index.tsx
+++ frontend/src/components/Title/index.tsx
@@ -5,10 +5,14 @@ interface TitleProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function Title({ label, children, ...props }: TitleProps) {
   return (
     <div {...props} className="flex flex-col items-center">
-      <h2 className={`font-title text-5xl text-center`} {...props}>
+      <h2 className={`font-title text-4xl text-center lg:text-5xl`} {...props}>
         {children}
       </h2>
-      {label && <label className="text-secondary text-center text-md">{label}</label>}
+      {label && (
+        <label className="text-secondary text-center lg:text-lg text-md">
+          {label}
+        </label>
+      )}
     </div>
   );
 }
