94fb03c902e6e4b637fa56993489bd57cfd0b4ad
Luiss1569 <rcsrcs1569@gmail.com>
2023-07-15 17:56:29 -0300

    :recycle: finish specialties section

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+      <p className="text-gray-900 text-xl mt-5">Carregando...</p>
+    </div>
+  );
+}
--- frontend/src/views/Home/Carrossel/index.tsx
+++ frontend/src/views/Home/Carrossel/index.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 export default function Carrossel() {
   return (
-    <section className="flex flex-col justify-center lg:items-center relative lg:flex-row">
-      <div className="flex flex-col justify-around lg:items-start items-end h-96">
-        <h1 className="font-mono text-4xl lg:text-5xl">
+    <section className="flex flex-col justify-center md:items-center relative md:flex-row py-0">
+      <div className="flex flex-col justify-around md:items-start items-end h-80">
+        <h1 className="font-mono text-4xl md:text-5xl">
           Comida <strong className="text-primary">saudável</strong> também pode
           ter <strong className="text-secondary">sabor</strong>.
         </h1>
-        <p className="text-lg lg:text-xl">
+        <p className="text-lg md:text-xl">
           Transforme seu corpo e sua vida com os sabores irresistíveis dos
           nossos pratos.
         </p>
         <Button variant="secondary">Veja o cardápio</Button>
       </div>
-      <div className="flex justify-center w-full mt-5 lg:mt-0 lg:w-2/3 m-auto">
+      <div className="flex justify-center w-full mt-5 md:mt-0 md:w-2/3 m-auto">
         <Image src={cmgCarrossel} alt="carrossel" />
       </div>
     </section>
