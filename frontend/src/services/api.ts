60efda28112f8e61d0f8a4fcab808cc3d5a31d9c
Daniel Enrique <danigon1998@gmail.com>
2023-07-16 18:15:44 -0300

    Mudanca de localhost para http://127.0.0.1:1337

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import Product from "@interfaces/product";
 
 const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
 
-const BASE_URL = process.env.STRAPI_URI + "/api" || "http://localhost:1337/api";
+const BASE_URL = process.env.STRAPI_URI + "/api" || "http://127.0.0.1:1337/api";
 
 const headers = new Headers({
   "Content-Type": "application/json",
